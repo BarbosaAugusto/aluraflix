87a28a793e46e6bde14800ecfdd00d9a1fd71f69
Augusto Barbosa <110637596+BarbosaAugusto@users.noreply.github.com>
2023-08-23 05:59:41 -0300

    Componente Menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Menu from './components/Menu/'
 
 function App() {
   return (
     <div>
-      Hello World
+      <Menu />
+      <h1>Hello World</h1>
     </div>
   );
 }
